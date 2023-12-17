cc3894e344917691c993308464ea2641e92fa210
Adarsh <adarshraj3309@gmail.com>
2023-12-17 19:01:42 +0530

    udated order route as per instructions

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -1,27 +1,28 @@
 const pool = require("../config/database");
+const catchAsyncErrors = require("../middleware/catchAsyncErrors");
 
-exports.getAllOrders = async (req, res) => {
-  const [rows] = await pool.query(`
-    SELECT *
-    FROM order_header`);
-  res.send(rows);
-};
+exports.getAllOrders = catchAsyncErrors(async (req, res) => {
+  const [Orders] = await pool.query(`SELECT * FROM order_header`);
+  const Length = Orders.length;
+  res.send({
+    Length,
+    Orders,
+  });
+});
 
-exports.getOrder = async (req, res) => {
+exports.getOrder = catchAsyncErrors(async (req, res) => {
   const order_id = req.params.id;
-
   const [rows] = await pool.query(
-    `
-    SELECT *
-    FROM order_header
-    WHERE order_id = ?`,
+    `SELECT * FROM order_header WHERE order_id = ?`,
     [order_id]
   );
-  res.send(rows);
-};
-exports.createOrder = async (req, res) => {
+  const order = rows[0];
+  delete order.ORDER_ID;
+  res.send(order);
+});
+
+exports.createOrder = catchAsyncErrors(async (req, res) => {
   const {
-    order_id,
     order_name,
     placed_date,
     aprv_date,
@@ -33,10 +34,26 @@ exports.createOrder = async (req, res) => {
     completed_date,
   } = req.body;
 
+  const d = new Date();
+  const order_id =
+    "" +
+    d.getDate() +
+    "" +
+    (d.getMonth() + 1) +
+    "" +
+    d.getFullYear() +
+    "" +
+    d.getHours() +
+    "" +
+    d.getMinutes() +
+    "" +
+    d.getSeconds();
+
   const [rows] = await pool.query(
-    `INSERT INTO order_header 
-    (ORDER_ID, ORDER_NAME, PLACED_DATE, APPROVED_DATE, STATUS_ID, CURRENCY_UOM_ID, PRODUCT_STORE_ID, SALES_CHANNEL_ENUM_ID, GRAND_TOTAL, COMPLETED_DATE)
-     VALUES(?,?,?,?,?,?,?,?,?,?)`,
+    `INSERT INTO order_header
+    (ORDER_ID, ORDER_NAME, PLACED_DATE, APPROVED_DATE, STATUS_ID, CURRENCY_UOM_ID,
+    PRODUCT_STORE_ID, SALES_CHANNEL_ENUM_ID, GRAND_TOTAL, COMPLETED_DATE)
+    VALUES (?,?,?,?,?,?,?,?,?,?)`,
     [
       order_id,
       order_name,
@@ -51,9 +68,9 @@ exports.createOrder = async (req, res) => {
     ]
   );
   res.send(rows);
-};
+});
 
-exports.updateOrder = async (req, res) => {
+exports.updateOrder = catchAsyncErrors(async (req, res) => {
   const order_id = req.params.id;
   const {
     order_name,
@@ -66,21 +83,10 @@ exports.updateOrder = async (req, res) => {
     grand_total,
     completed_date,
   } = req.body;
-
   const [rows] = await pool.query(
-    `UPDATE order_header
-    SET
-    ORDER_NAME=?, 
-    PLACED_DATE=?, 
-    APPROVED_DATE=?, 
-    STATUS_ID=?, 
-    CURRENCY_UOM_ID=?, 
-    PRODUCT_STORE_ID=?,
-     SALES_CHANNEL_ENUM_ID=?, 
-    GRAND_TOTAL=?,
-    COMPLETED_DATE=?
-    WHERE order_id = ?
-    `,
+    `UPDATE order_header SET
+    ORDER_NAME=?,PLACED_DATE=?,APPROVED_DATE=?,STATUS_ID=?,CURRENCY_UOM_ID=?,PRODUCT_STORE_ID=?,
+    SALES_CHANNEL_ENUM_ID=?,GRAND_TOTAL=?,COMPLETED_DATE=? WHERE order_id=?`,
     [
       order_name,
       placed_date,
@@ -95,4 +101,4 @@ exports.updateOrder = async (req, res) => {
     ]
   );
   res.send(rows);
-};
+});
--- backend/routes/orderRoute.js
+++ backend/routes/orderRoute.js
@@ -1,12 +1,15 @@
-const express = require('express');
+const express = require("express");
 
 const {
-    createOrder,getAllOrders,getOrder,updateOrder
+  createOrder,
+  getAllOrders,
+  getOrder,
+  updateOrder,
 } = require("../controllers/orderController");
 
 const router = express.Router();
 
-router.route('/orders').get(getAllOrders);
+router.route("/orders").get(getAllOrders);
 router.route("/order/:id").get(getOrder);
 router.route("/order/new").post(createOrder);
 router.route("/order/update/:id").put(updateOrder);
