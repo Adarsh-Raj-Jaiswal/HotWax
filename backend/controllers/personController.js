331c688f5b4e051f4a4b4336e47c252131e13fc7
Adarsh <adarshraj3309@gmail.com>
2023-12-17 18:23:05 +0530

    person api done removed update bug also added error handling

--- backend/controllers/personController.js
+++ backend/controllers/personController.js
@@ -1,38 +1,58 @@
-const pool = require('../config/database')
+const pool = require("../config/database");
+const catchAsyncErrors = require("../middleware/catchAsyncErrors");
 
-exports.getPerson = async (req, res) => {
-    const id = req.params.id;
-    const [rows] = await pool.query(`
-    SELECT *
-    FROM person
-    WHERE party_id = ?
-    `, [id]);
-    res.send(rows[0]);
-};
+exports.getPersons = catchAsyncErrors(async (req, res) => {
+  const [Persons] = await pool.query(`SELECT * FROM person`);
+  const Length = Persons.length;
+  res.send({
+    Length,
+    Persons,
+  });
+});
 
-exports.createPerson = async (req, res) => {
+exports.getPerson = catchAsyncErrors(async (req, res) => {
+  const id = req.params.id;
+  const [rows] = await pool.query(`SELECT * FROM person WHERE party_id = ?`, [
+    id,
+  ]);
+  res.send(rows[0]);
+});
 
-    const partyId = req.params.id;
-    const [rows] = await pool.query(`INSERT INTO person (PARTY_ID)VALUES (?);`
-        , [partyId]);
-    res.send(rows);
-};
+exports.createPerson = catchAsyncErrors(async (req, res) => {
+  const partyId = req.params.id;
+  const [rows] = await pool.query(`INSERT INTO person (PARTY_ID)VALUES (?);`, [
+    partyId,
+  ]);
+  res.send(rows);
+});
 
-exports.updatePerson = async (req, res) => {
-    const partyId = req.params.id;
-    const { salutation, first_name, middle_name, last_name, gender, birth_date, marital_status, empl_status, occupation } = req.body;
-    const [rows] = await pool.query(`UPDATE person
-    SET
-      SALUTATION = ?,
-      FIRST_NAME = ?,
-      MIDDLE_NAME = ?,
-      LAST_NAME = ?,
-      GENDER = ?,
-      BIRTH_DATE = ?,
-      MARITAL_STATUS_ENUM_ID = ?,
-      EMPLOYMENT_STATUS_ENUM_ID = ?,
-      OCCUPATION = ?
-    WHERE PARTY_ID = ?
-    ` , [salutation, first_name, middle_name, last_name, gender, birth_date, marital_status, empl_status, occupation,partyId]);
-    res.send(rows);
-};
+exports.updatePerson = catchAsyncErrors(async (req, res) => {
+  const partyId = req.params.id;
+  const {
+    SALUTATION,
+    FIRST_NAME,
+    MIDDLE_NAME,
+    LAST_NAME,
+    GENDER,
+    BIRTH_DATE,
+    MARITAL_STATUS,
+    EMPL_STATUS,
+    OCCUPATION,
+  } = req.body;
+  const [rows] = await pool.query(
+    `UPDATE person SET SALUTATION = ?,FIRST_NAME = ?,MIDDLE_NAME = ?,LAST_NAME = ?,GENDER = ?,BIRTH_DATE = ?,MARITAL_STATUS_ENUM_ID = ?,EMPLOYMENT_STATUS_ENUM_ID = ?,OCCUPATION = ? WHERE PARTY_ID = ?`,
+    [
+      SALUTATION,
+      FIRST_NAME,
+      MIDDLE_NAME,
+      LAST_NAME,
+      GENDER,
+      BIRTH_DATE,
+      MARITAL_STATUS,
+      EMPL_STATUS,
+      OCCUPATION,
+      partyId,
+    ]
+  );
+  res.send(rows);
+});
--- backend/routes/personRoute.js
+++ backend/routes/personRoute.js
@@ -1,12 +1,16 @@
-const express = require('express');
+const express = require("express");
 
 const {
-    getPerson, createPerson, updatePerson
+  getPerson,
+  createPerson,
+  updatePerson,
+  getPersons,
 } = require("../controllers/personController");
 
 const router = express.Router();
 
 router.route("/person/:id").get(getPerson);
+router.route("/persons").get(getPersons);
 router.route("/person/new/:id").post(createPerson);
 router.route("/person/update/:id").put(updatePerson);
 
