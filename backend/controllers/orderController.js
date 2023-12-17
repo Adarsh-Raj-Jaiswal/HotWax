38497fd0e10e22041893053f3eba42e305707d44
Adarsh <adarshraj3309@gmail.com>
2023-12-17 20:20:23 +0530

    added encryption on credit card info

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -1,9 +1,17 @@
 const pool = require("../config/database");
 const catchAsyncErrors = require("../middleware/catchAsyncErrors");
+const Cryptr = require("cryptr");
+const cryptr = new Cryptr("myTotallySecretKey");
 
 exports.getAllOrders = catchAsyncErrors(async (req, res) => {
   const [Orders] = await pool.query(`SELECT * FROM order_header`);
   const Length = Orders.length;
+
+  for (let i = 0; i < Length; i++) {
+    const credit_card = cryptr.decrypt(Orders[i].CREDIT_CARD);
+    Orders[i].CREDIT_CARD = credit_card;
+  }
+  
   res.send({
     Length,
     Orders,
@@ -17,6 +25,8 @@ exports.getOrder = catchAsyncErrors(async (req, res) => {
     [order_id]
   );
   const order = rows[0];
+  const credit_card = cryptr.decrypt(order.CREDIT_CARD);
+  order.CREDIT_CARD = credit_card;
   delete order.ORDER_ID;
   res.send(order);
 });
@@ -32,7 +42,10 @@ exports.createOrder = catchAsyncErrors(async (req, res) => {
     sales_channel,
     grand_total,
     completed_date,
+    credit_card,
   } = req.body;
+
+  const encryptCreditCard = cryptr.encrypt(credit_card);
 
   const d = new Date();
   const order_id =
@@ -52,8 +65,8 @@ exports.createOrder = catchAsyncErrors(async (req, res) => {
   const [rows] = await pool.query(
     `INSERT INTO order_header
     (ORDER_ID, ORDER_NAME, PLACED_DATE, APPROVED_DATE, STATUS_ID, CURRENCY_UOM_ID,
-    PRODUCT_STORE_ID, SALES_CHANNEL_ENUM_ID, GRAND_TOTAL, COMPLETED_DATE)
-    VALUES (?,?,?,?,?,?,?,?,?,?)`,
+    PRODUCT_STORE_ID, SALES_CHANNEL_ENUM_ID, GRAND_TOTAL, COMPLETED_DATE,CREDIT_CARD)
+    VALUES (?,?,?,?,?,?,?,?,?,?,?)`,
     [
       order_id,
       order_name,
@@ -65,6 +78,7 @@ exports.createOrder = catchAsyncErrors(async (req, res) => {
       sales_channel,
       grand_total,
       completed_date,
+      encryptCreditCard,
     ]
   );
   res.send(rows);
